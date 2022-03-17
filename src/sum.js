3452644d72657505f5b5faa5d572752f951a65e8
Emerson Henning <emerson@henning.com.br>
2022-03-17 16:34:46 -0300

    feat: add sub, mult functions

--- src/sum.js
+++ src/sum.js
@@ -2,4 +2,14 @@ function sum(a,b) {
     return a + b;
 }
 
-module.exports = sum;
+function sub(a,b) {
+    return a - b;
+}
+
+function times(a,b) {
+    return a * b;
+}
+
+
+
+module.exports = sum, sub, times;
