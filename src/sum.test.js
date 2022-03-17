862b43491003e41e406089c37cff72e6a6e7dd71
Emerson Henning <emerson@henning.com.br>
2022-03-17 16:48:19 -0300

    feat: add div function and move to a class

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,20 @@
-const sum = require('./sum')
+const Calc = require('./Calc');
+
+
+const calc = new Calc()
 
 test("add 1+ 2 to be equal 3", () => {
-    expect(sum(1,2)).toBe(3);
+    expect(calc.sum(1,2)).toBe(3);
+});
+
+test("sub 3 -  2 to be equal 1", () => {
+    expect(calc.sub(3,2)).toBe(1);
+});
+
+test("times  3 *  2 to be equal 6", () => {
+    expect(calc.times(3,2)).toBe(6);
+});
+
+test("div  8 *  2 to be equal 4", () => {
+    expect(calc.div(8,2)).toBe(4);
 });
